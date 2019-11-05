c42ec6ceaf25761dedf7dd341a495a5df86cb44f
RAIERIN <erinrai5874@gmail.com>
2019-11-05 18:25:15 +0545

    Used AnimatedWidget class provided by flutter

---
+++ lib/animation_widget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class AnimatedWidgetView extends StatefulWidget {
+  AnimatedWidgetView({Key key}) : super(key: key);
+
+  _BasicAnimationViewState createState() => _BasicAnimationViewState();
+}
+
+class _BasicAnimationViewState extends State<AnimatedWidgetView>
+    with TickerProviderStateMixin {
+  AnimationController controller;
+  Animation growAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 3));
+    controller..forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: GrowingContainer(
+        controller: controller,
+      )),
+    );
+  }
+}
+
+class GrowingContainer extends AnimatedWidget {
+  GrowingContainer({AnimationController controller})
+      : super(
+            listenable: Tween<double>(begin: 0, end: 200).animate(controller));
+  @override
+  Widget build(BuildContext context) {
+    Animation<double> animation = listenable;
+    return Container(
+      child: Container(
+        height: animation.value,
+        width: animation.value,
+        color: Colors.red,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:animation_guide/animation_widget.dart';
 import 'package:flutter/material.dart';
 import './basic_animation.dart';
 
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Basic Animation',
-      home: BasicAnimationView(),
+      home: AnimatedWidgetView(),
     );
   }
 }
